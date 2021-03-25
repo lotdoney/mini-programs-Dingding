c95c6265f030f8e31539bd2d3af46ab5eec23657
lotdoney <619579657@qq.com>
2021-03-25 20:46:02 +0800

    提交03

---
+++ emos-wechat/pages/demo/demo.vue
@@ -0,0 +1,37 @@
+<template>
+	<view>
+		<view> {{username}} </view>
+		<view v-for="one in tel">{{one}}</view>
+		<view v-if="age>=18">
+		<button @tap="signUp()">我要报名</button>
+		</view>
+		<view> 
+		<input type="text" v-model="address"  placeholder="输入地址"/>
+		</view>
+		<view> {{address}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				username:"Doney",
+				tel:['1223423424','1231564342'],
+				age:23,
+				address:""
+			}
+		},
+		methods: {
+			signUp:function(){
+				uni.showToast({
+					title:"点击了报名按钮"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ emos-wechat/pages/register/register.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<image src="../../static/logo-2.png" mode="widthFix" class="logo"></image>
+		<view class="register-container">
+			<input type="text" placeholder="输入你的邀请码" class="register-code" 
+			maxlength="6" v-model="registerCode"/>
+		</view>
+		<view class="register-desc">管理员创建员工证账号之后，你可以从你的个人邮箱中获得注册邀请码</view>
+		<button class="register-btn" open-type="getUserInfo @tap=register()">执行注册</button>
+	</view>
+</template>
+ 
+<script>
+	export default {
+		data() {
+			return {
+				registerCode: null
+			};
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("register.less");
+</style>
